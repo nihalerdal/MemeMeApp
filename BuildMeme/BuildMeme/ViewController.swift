716881aa2f1bab7b4224a9221fce52df5366d343
Nihal Erdal <nihalerdall@gmail.com>
2021-01-28 17:27:26 -0800

    implemented keyboard notifications for moving the view up and down

--- BuildMeme/BuildMeme/ViewController.swift
+++ BuildMeme/BuildMeme/ViewController.swift
@@ -14,18 +14,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var textFieldTop: UITextField!
     @IBOutlet weak var textFieldButtom: UITextField!
     
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [NSAttributedString.Key.strokeColor: UIColor.black,
+                                                             NSAttributedString.Key.foregroundColor: UIColor.white,
+                                                             NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+                                                             NSAttributedString.Key.strokeWidth:  -4.0
+    ]
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+         super.viewDidLoad()
         
         textFieldTop.delegate = self
         textFieldButtom.delegate = self
-    }
+  
+         textFieldTop.text = "TOP"
+         textFieldButtom.text = "BOTTOM"
+  
+         textFieldTop.defaultTextAttributes = memeTextAttributes
+         textFieldButtom.defaultTextAttributes = memeTextAttributes
+ 
+         textFieldTop.textAlignment = .center
+         textFieldButtom.textAlignment = .center
+     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        //**1** Sign up to be notified when the keyboard appears
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+        
     }
     
-  // 1.When the user tap the Pick button, it provide the open photo gallery(picker controller)
+    override func viewWillDisappear(_ animated: Bool) {
+
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    
+    // 1.When the user tap the Pick button, it provide the open photo gallery(picker controller)
     @IBAction func pickAnImageFromGallery(_ sender: Any) {
         
         let imagePicker = UIImagePickerController()
@@ -75,8 +105,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return true
     }
    
+    //**2**
+    func subscribeToKeyboardNotifications() {
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
     
     
+    //**3** When the keyboardWillShow notification is received, shift the view's frame up
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        
+        if textFieldButtom.isEditing { //-> only for buttom text
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        
+        view.frame.origin.y = 0
+    }
 
 
     /*
